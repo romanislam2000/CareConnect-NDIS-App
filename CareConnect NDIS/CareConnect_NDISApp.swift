e5a54c666c002a8d5b1a0a7409e2904c5e2901a8
Md Roman Islam <mdroman.islam@student.uts.edu.au>
2025-05-14 01:41:12 +1000

    Initial Commit

---
+++ CareConnect NDIS/CareConnect_NDISApp.swift
@@ -0,0 +1,17 @@
+//
+//  CareConnect_NDISApp.swift
+//  CareConnect NDIS
+//
+//  Created by Md Roman Islam on 14/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct CareConnect_NDISApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
